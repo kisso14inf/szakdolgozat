dd20145222547ee35d1f5696c9a0511d9bd1cd08
Kiss Ottó Kristóf <60258898+kisso14inf@users.noreply.github.com>
2020-02-11 21:29:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+    
+    require_once "iranyito/funkciok.php";
+    require_once "iranyito/kontrollerek.php";
+    require_once "iranyito/konfiguracio.php"; //alap beállítások helye
+    require_once "iranyito/app.php"; //algoritmusok helye
+    require_once "oldalak/gyujto.php";
+?>
